24ea134268aed92a7ecffb532be55b15957d4c43
athul.krishna@qburst.com <athul.krishna@qburst.com>
2023-07-02 20:35:57 +0530

    BE services for job listing-initial commit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const { authenticationMiddleware } = require("./middleware/auth");
 const connectDB = require("./db/connect");
 
 // middleware
-app.use(express.static("./public"));
+
 app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000, // 15 minutes
@@ -29,6 +29,9 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+app.get("/", (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticationMiddleware, jobsRouter);
 app.use(notFoundMiddleware);
